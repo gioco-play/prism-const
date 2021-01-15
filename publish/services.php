038139c961090e0365ec9748cda140f73f1a6380
Xuan Yang <xuan.yang@goldenf.co>
2021-01-15 10:37:11 +0800

    update services.php

--- publish/services.php
+++ publish/services.php
@@ -1,16 +1,24 @@
 <?php
 return [
-    'consumers' => [
-        [
-            'name' => 'TransactionService',
-            'service' => \GiocoPlus\Transaction\Contract\TransactionServiceInterface::class,
-            'protocol' => 'jsonrpc-http',
-            'load_balancer' => 'random',
-            // 這個消費者要從哪個服務中心獲取節點資訊，如不配置則不會從服務中心獲取節點資訊
-            'registry' => [
-                'protocol' => 'consul',
-                'address' => env('CONSUL_HOST'),
-            ]
-        ]
-    ],
+    'consumers' => value(function(){
+        $consumers = [];
+
+        $services = [
+            'TransactionService' => \GiocoPlus\Transaction\Contract\TransactionServiceInterface::class,
+            'RecordService' => \GiocoPlus\Record\Contract\RecordServiceInterface::class
+        ];
+        foreach ($services as $name => $interface) {
+            $consumers[] = [
+                'name' => $name,
+                'service' => $interface,
+                'protocol' => 'jsonrpc-http',
+                'load_balancer' => 'random',
+                // 這個消費者要從哪個服務中心獲取節點資訊，如不配置則不會從服務中心獲取節點資訊
+                'registry' => [
+                    'protocol' => 'consul',
+                    'address' => env('CONSUL_HOST'),
+                ]
+            ];
+        }
+    }),
 ];
